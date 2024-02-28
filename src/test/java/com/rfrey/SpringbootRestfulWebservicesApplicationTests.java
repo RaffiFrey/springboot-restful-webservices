21e735228efd40a8e1f764f5f3852fe3da50d9c1
RaphaelFrey <raphael.marius.frey@gmail.com>
2024-02-28 16:06:42 +0100

    Initial commit

---
+++ src/test/java/com/rfrey/SpringbootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
